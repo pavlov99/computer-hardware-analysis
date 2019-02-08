9751f7155408dd9e2051868bf27202cafd786b0b
Kirill Pavlov <k@p99.io>
2019-02-08 23:16:27 +0800

    ADD: crawl gpu data

--- src/crawler.py
+++ src/crawler.py
@@ -49,4 +49,39 @@ def crawl_cpus():
 
     logger.info('Successfully crawled {:,} CPU records'.format(num_rows))
 
-crawl_cpus()
+
+def crawl_gpus():
+    output_filename = path.join(DATA_FOLDER, 'gpus.csv')
+    header = ['name', 'passmark', 'rank', 'value', 'price']
+    logger.debug('Request GPU html page')
+
+    gpu_data = urlopen(GPU_LIST_URL, context=ssl_context).read()
+    logger.debug('Parse GPU html page')
+    soup = BeautifulSoup(gpu_data, 'html.parser')
+
+    num_rows = 0
+    with open(output_filename, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(
+            csvfile,
+            dialect=csv.unix_dialect,
+            quoting=csv.QUOTE_MINIMAL
+        )
+        csvwriter.writerow(header)
+
+        for cpu_html in soup.find_all('tr', id=re.compile('gpu')):
+            row = [t.get_text() for t in cpu_html.find_all('td')]
+            assert len(row) == len(header)
+
+            price_index = header.index('price')
+            price = row[price_index]
+            row[price_index] = None if price == 'NA' \
+                else float(price.lstrip('$').rstrip('*').replace(',', ''))
+            csvwriter.writerow(row)
+            num_rows += 1
+
+    logger.info('Successfully crawled {:,} GPU records'.format(num_rows))
+
+
+if __name__ == '__main__':
+    crawl_cpus()
+    crawl_gpus()
